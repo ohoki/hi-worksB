3b1fb7ce1bf40a34e3c009d96d459496805890b0
admin <admin@YD02-11>
2023-08-11 17:23:03 +0900

    공지사항 페이징

--- worksB/src/main/java/com/worksb/hi/notice/mapper/NoticeMapper.java
+++ worksB/src/main/java/com/worksb/hi/notice/mapper/NoticeMapper.java
@@ -2,9 +2,16 @@ package com.worksb.hi.notice.mapper;
 
 import java.util.List;
 
+import com.worksb.hi.common.PagingVO;
 import com.worksb.hi.notice.service.NoticeVO;
 
 public interface NoticeMapper {
+	
+	// 페이징 게시물 총 갯수
+	public int getTotalCount();
+	
+	// 페이징 게시글 전체 조회
+	public List<NoticeVO> selectNoticeAll(PagingVO pagingVO);
 	
 	// 공지 목록
 	public List<NoticeVO> getNoticeList();
@@ -13,11 +20,11 @@ public interface NoticeMapper {
 	public NoticeVO getNoticeInfo(NoticeVO noticeVO);
 	
 	// 공지 등록
-	public int insertNotice(NoticeVO noticeVO);
+	public int noticeInsert(NoticeVO noticeVO);
 	
 	// 공지 수정
-	public int updateNotice(NoticeVO noticeVO);
+	public int noticeUpdate(NoticeVO noticeVO);
 	
 	// 공지 삭제
-	public int deleteNotice(int noticeId);
+	public int noticeDelete(int noticeId);
 }
