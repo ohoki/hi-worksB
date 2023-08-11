607c4f096a3703dc9a2c8bfa9bc7301265036f9f
admin <admin@YD02-15>
2023-08-11 17:14:39 +0900

    개인스케줄 서비스작성

---
+++ worksB/src/main/java/com/worksb/hi/mycalendar/mapper/PrivateScheMapper.java
@@ -0,0 +1,23 @@
+package com.worksb.hi.mycalendar.mapper;
+
+import java.util.List;
+
+import com.worksb.hi.mycalendar.service.PrivateScheVO;
+
+public interface PrivateScheMapper {
+	
+	//단건조회
+	public PrivateScheVO selectPsche(PrivateScheVO privateScheVO);
+	
+	//전체조회
+	public List<PrivateScheVO> selectAllPche(PrivateScheVO privateScheVO);
+	
+	//일정등록
+	public int insertPsche(PrivateScheVO privateScheVO);
+	
+	//일정수정
+	public int updatePsche(PrivateScheVO privateScheVO);
+	
+	//일정삭제
+	public int deletePsche(PrivateScheVO privateScheVO);
+}
