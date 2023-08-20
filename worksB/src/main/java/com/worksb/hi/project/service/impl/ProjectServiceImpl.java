b6055fdbf6a91b4fd52500d5fccf6230dbae22d6
jh <uoo.jns@gmail.com>
2023-08-20 22:07:46 +0900

    이게맞을걸? serviceImpl에서 result.add를 밑에 두었음

--- worksB/src/main/java/com/worksb/hi/project/service/impl/ProjectServiceImpl.java
+++ worksB/src/main/java/com/worksb/hi/project/service/impl/ProjectServiceImpl.java
@@ -99,11 +99,9 @@ public class ProjectServiceImpl implements ProjectService {
 		for(int i=vo.size()-1;i >= 0;i--){
 		    ProjectVO project = vo.get(i);
 
-		    // project_id가 중복되지 않으면 결과 리스트에 추가하고, 중복된 id는 uniqueProjectIds에 추가
 		    if (!uniqueProjectIds.contains(project.getProjectId())) {
 		        uniqueProjectIds.add(project.getProjectId());
 
-		        // 세션의 id와 member_id가 일치하는 경우 해당 프로젝트의 project_id를 sessionMatchingProjectIds에 추가
 		        if (memberId.equals(project.getMemberId())) {
 		            sessionMatchingProjectIds.add(project.getProjectId());
 		        }
@@ -113,8 +111,8 @@ public class ProjectServiceImpl implements ProjectService {
 		// 결과 리스트에 중복된 프로젝트 중 세션과 일치하는 것만 추가
 		for(ProjectVO project:vo) {
 		    if (uniqueProjectIds.contains(project.getProjectId())) {
-		        result.add(project);
 		        uniqueProjectIds.remove(project.getProjectId()); // 중복 제거한 프로젝트는 세트에서 제거
+		        result.add(project);
 		    }
 		}
 
